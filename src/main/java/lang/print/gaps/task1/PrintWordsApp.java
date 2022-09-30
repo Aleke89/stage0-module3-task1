d77309e9096b60cb0bd7ffa3bd00fe8ff38c05e1
Aleke <alnur.rysbek@mail.ru>
2022-09-30 14:25:42 +0600

    reshil

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,8 +2,6 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        System.out.print("java");
-        System.out.println("is");
-        System.out.println("smth");
+        System.out.print("java \nis \nsmth");
     }
 }
--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task1;
 
 public class UpdatedApp {
     public static void main(String[] args) {
-        System.out.print("I am updated");
-        System.out.println("can be compiled. And can write to console");
+        System.out.print("I am updated \ncan be compiled. And can write to console");
+
     }
 }
