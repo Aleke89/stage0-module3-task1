b95b0f8b1fe51b1cfa283ce0aca4eb4efc3fd6b5
Aleke <alnur.rysbek@mail.ru>
2022-09-30 14:29:55 +0600

    reshil3

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task1;
 
 public class UpdatedApp {
     public static void main(String[] args) {
-        System.out.print("I am updated \ncan be compiled. And can write to console");
+        System.out.print("I'm updated \ncan be compiled. And can write to console");
 
     }
 }
